b73cfcd835620db23206bc0b632652709de97db8
Muji-47it <107984150+Muji-47it@users.noreply.github.com>
2022-06-24 17:23:38 +0500

    Update arrayassignment.cpp

--- arrayassignment.cpp
+++ arrayassignment.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 int main() {
     // Write C++ code here
-int size;
-    int arr[size]={};
-    cin>>size;
+    int size;
+        cin>>size;
+    int arr[size]={1,3,3};
+    cin>>arr[size-1] ;
     for(int i=0;i<size;i++)
-    cin>>arr[size-1] ; 
-    cout<< arr[size-1]; 
+    cout<< arr[i]; 
 }
