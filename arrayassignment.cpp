701f4c54e27b3fa004583c1e49e272eb037f3553
Muji-47it <107984150+Muji-47it@users.noreply.github.com>
2022-06-24 17:23:04 +0500

    Add files via upload

---
+++ arrayassignment.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+int size;
+    int arr[size]={};
+    cin>>size;
+    for(int i=0;i<size;i++)
+    cin>>arr[size-1] ; 
+    cout<< arr[size-1]; 
+}
